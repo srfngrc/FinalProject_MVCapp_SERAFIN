4516ef6f16ce99574cc4337d8952ce40a10c9154
srfngrc <srfngrc@gmail.com>
2019-08-12 20:55:47 -0400

    ok. all 2 models, 2 controllers and 1 first view with layout already created aug12th 8h55pm

---
+++ Models/TaxSystemOperationMODEL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject_MVCapp_SERAFIN.Models
+{
+    public class TaxSystemOperationMODEL
+    {
+        int operationId;
+        string isin;
+        DateTime purchaseDate;
+        DateTime sellDate;
+        string amount;
+        string description;
+    }
+}
---
+++ Models/TaxSystemUsersMODEL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject_MVCapp_SERAFIN.Models
+{
+    public class TaxSystemUsersMODEL
+    {
+        int loginId;
+        string userName;
+        string passWord;
+        string description;
+        string isAdmin;
+    }
+}
