5ae8003da6fda7a71046c6eaec24240b31078a3e
srfngrc <srfngrc@gmail.com>
2019-08-19 20:56:31 -0400

    last version of today 8h56pm

--- Controllers/TaxSystemOperationController.cs
+++ Controllers/TaxSystemOperationController.cs
@@ -14,48 +14,60 @@ namespace FinalProject_MVCapp_SERAFIN.Controllers
         // GET: TaxSystemOperation
         public ActionResult Manage()
         {
-            //List<TaxSystemOperationMODEL> ListOfOperations = new List<TaxSystemOperationController>;
-            //ListOfOperations = ReadAllOperationsFromDB();
-            //return View("Manage", ListOfOperations);
-            return View();
+            List<TaxSystemOperationMODEL> ListOfOperations = new List<TaxSystemOperationMODEL>();
+            ListOfOperations = ReadAllOperationsFromDB();
+            return View("Manage", ListOfOperations);
+            //return View();
         }
 
-        //static List<TaxSystemOperationMODEL> ReadAllOperationsFromDB()
-        //{
-        //    List<TaxSystemOperationMODEL> myOperations = new List<TaxSystemOperationMODEL>();
-        //    SqlConnection myConnSRFN2 = new SqlConnection();
-        //    try
-        //    {
-        //        myConnSRFN2.ConnectionString = ConfigurationManager.ConnectionStrings["SRFNconnection"].ConnectionString;
-        //        myConnSRFN2.Open();
+        static List<TaxSystemOperationMODEL> ReadAllOperationsFromDB()
+        {
+            List<TaxSystemOperationMODEL> myOperations = new List<TaxSystemOperationMODEL>();
+            SqlConnection myConnSRFN2 = new SqlConnection();
+            try
+            {
+                myConnSRFN2.ConnectionString = ConfigurationManager.ConnectionStrings["SRFNconnection"].ConnectionString;
+                myConnSRFN2.Open();
 
-        //        string queryString = "SELECT operationId,isin,purchaseDate,sellDate,amount,description FROM Nutella.operations;";
-        //        SqlCommand commandReadAll = new SqlCommand(queryString, myConnSRFN2);
+                string queryString = "SELECT operationId,isin,purchaseDate,sellDate,amount,description FROM Nutella.operations;";
+                SqlCommand commandReadAll = new SqlCommand(queryString, myConnSRFN2);
 
-        //        SqlDataReader myUsersResults = commandReadAll.ExecuteReader();
-        //        while (myUsersResults.Read())
-        //        {
-        //            TaxSystemOperationMODEL newOperation = new TaxSystemOperationMODEL();
-        //            newOperation.operationId = int.Parse(myUsersResults["loginId"].ToString());
-        //            newOperation.isin = myUsersResults["userName"].ToString();
-        //            newOperation.purchaseDate = myUsersResults["passWord"].ToString();
-        //            newOperation.sellDate = myUsersResults["description"].ToString();
-        //            newOperation.amount = myUsersResults["isAdmin"].ToString();
-        //            newOperation.description = myUsersResults["isAdmin"].ToString();
-        //            myOperations.Add(newOperation);
-        //        }
+                SqlDataReader myUsersResults = commandReadAll.ExecuteReader();
+                while (myUsersResults.Read())
+                {
+                    TaxSystemOperationMODEL newOperation = new TaxSystemOperationMODEL();
+                    newOperation.operationId = int.Parse(myUsersResults["operationId"].ToString());
+                    newOperation.isin = myUsersResults["isin"].ToString();
 
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return null;
-        //    }
-        //    finally
-        //    {
-        //        myConnSRFN2.Close();
-        //    }
-        //    return myOperations;
-        //}
+                    newOperation.purchaseDate = (DateTime)myUsersResults["purchaseDate"];
+
+                    //DateTime aa = Convert.ToDateTime(myUsersResults["sellDate"]);
+                    //newOperation.sellDate = aa.ToString("yyyy-mm-dd");
+                    newOperation.sellDate = Convert.ToDateTime(myUsersResults["sellDate"]);
+
+                    //el mas correcto: el de selldate pero con Tostring("YYYY-MM-dd") al final
+                    //newOperation.sellDate = Convert.ToDateTime(myUsersResults["sellDate"]).ToString("YYYY-MM-dd HH:mm:ss");
+
+                    //newOperation.purchaseDate = Convert.ToDateTime(myUsersResults["purchaseDate"]).ToString("dd/MM/yyyy");
+                    //newOperation.purchaseDate = DateTime.Parse(myUsersResults["purchaseDate"].ToString);
+                    //newOperation.purchaseDate = GetValue<DateTime>(myUsersResults["purchaseDate"]);
+
+                    newOperation.amount = myUsersResults["amount"].ToString();
+                    newOperation.description = myUsersResults["description"].ToString();
+                    myOperations.Add(newOperation);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                myConnSRFN2.Close();
+            }
+            return myOperations;
+        }
 
 
         // GET: TaxSystemOperation/Details/5
@@ -74,13 +86,40 @@ namespace FinalProject_MVCapp_SERAFIN.Controllers
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
+            TaxSystemOperationMODEL newOperation = new TaxSystemOperationMODEL();
+            SqlConnection connCREATEpost = new SqlConnection();
             try
             {
-                return RedirectToAction("Index");
+                connCREATEpost.ConnectionString = ConfigurationManager.ConnectionStrings["SRFNconnection"].ConnectionString;
+
+                newOperation.isin = collection["isin"];
+                newOperation.purchaseDate = Convert.ToDateTime(collection["purchaseDate"].ToString());
+                //newOperation.sellDate = collection["sellDate"].ToString("YYYY-MM-dd");
+                newOperation.amount = collection["amount"];
+                newOperation.description = collection["description"];
+
+                //string queryCREATE = "INSERT INTO Nutella.operations (isin,purchaseDate,sellDate,amount,description)" +
+                //    " VALUES ('" +
+                //    newOperation.isin + "','" +
+                //    newOperation.purchaseDate + "','" +
+                //    newOperation.sellDate + "','" +
+                //    newOperation.amount + "','" +
+                //    newOperation.description + "');";
+
+                connCREATEpost.Open();
+
+                //SqlCommand commCREATEpost = new SqlCommand(queryCREATE, connCREATEpost);
+                //int numRowsAffected = commCREATEpost.ExecuteNonQuery();
+                
+                return RedirectToAction("Manage");
             }
             catch
             {
                 return View();
+            }
+            finally
+            {
+                connCREATEpost.Close();
             }
         }
 
